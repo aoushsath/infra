d31ffef06ab767bf5d864e1ce9b9b3214cf67603
Tosh <git@chaoticenginerd.com>
2023-03-06 20:15:06 +0545

    Add neovim config

--- home.nix
+++ home.nix
@@ -43,4 +43,26 @@
       };
     };
   };
+
+  # Neovim
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      gruvbox-nvim
+      nvim-autopairs
+      nvim-lspconfig
+      nvim-treesitter
+      telescope-nvim
+    ];
+    extraPackages = with pkgs; [
+      nil
+      nodePackages.pyright
+      rust-analyzer
+      sumneko-lua-language-server
+    ];
+    extraLuaConfig = ''
+      ${ builtins.readFile ./nvim/init.lua }
+    '';
+  };
 }
---
+++ nvim/init.lua
@@ -0,0 +1,112 @@
+-- Settings --
+-- Editor UI
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "88"
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.mouse = ""
+vim.opt.termguicolors = true
+
+-- Editing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.list = true
+vim.opt.listchars:append { trail = "*" }
+vim.opt.completeopt = { "menuone", "noinsert" }
+
+-- Netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_liststyle = 3
+
+-- Leader
+vim.g.mapleader = " "
+
+-- Plugins --
+-- Autopairs
+require("nvim-autopairs").setup {}
+
+-- Treesitter
+require("nvim-treesitter.configs").setup {
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+-- LSP
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+local on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local nvim_lsp = require("lspconfig")
+local servers = {
+    "nil_ls",
+    "pyright",
+    "rust_analyzer",
+    "sumneko_lua",
+}
+
+for _, server in ipairs(servers) do
+    nvim_lsp[server].setup {
+        on_attach = on_attach,
+    }
+end
+
+-- Keymaps --
+-- General
+vim.keymap.set("n", "<leader>ww", "<cmd>write<cr>")
+vim.keymap.set("n", "<leader>be", "<cmd>quit<cr>")
+vim.keymap.set("n", "<leader><cr>", "<cmd>source %<cr>")
+
+-- Splits
+vim.keymap.set("n", "<leader>v", "<cmd>wincmd v<cr>")
+vim.keymap.set("n", "<leader>s", "<cmd>wincmd s<cr>")
+
+-- Splits navigation
+vim.keymap.set("n", "<leader>h", "<cmd>wincmd h<cr>")
+vim.keymap.set("n", "<leader>j", "<cmd>wincmd j<cr>")
+vim.keymap.set("n", "<leader>k", "<cmd>wincmd k<cr>")
+vim.keymap.set("n", "<leader>l", "<cmd>wincmd l<cr>")
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>ts", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>th", builtin.help_tags, {})
+
+-- Colorscheme
+vim.opt.background = "light"
+vim.cmd("colorscheme gruvbox")
