a5c274ef689f660f81f7fb3dc8d93fb45eeb2d42
Tosh <git@chaoticenginerd.com>
2023-03-28 11:44:17 +0545

    Add starship config

--- home.nix
+++ home.nix
@@ -166,4 +166,37 @@
     enableSyntaxHighlighting = true;
     enableAutosuggestions = true;
   };
+
+  # Starship
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$directory$git_status$character ";
+      directory = {
+        read_only = " [RO]";
+        truncation_length = 1;
+      };
+      character = {
+        error_symbol = "\\$";
+        success_symbol = "\\$";
+      };
+      git_branch = {
+        format = "\\([$branch(:$remote_branch)]($style)\\ )";
+        style = "purple";
+      };
+      git_status = {
+        format = "(\\[[$all_status$ahead_behind]($style)\\] )";
+        ahead = ">";
+        behind = "<";
+        deleted = "x";
+        diverged = "<>";
+        renamed = "r";
+        style = "red";
+      };
+      python = {
+        format = "(\\($virtualenv\\)) ";
+      };
+    };
+  };
 }
